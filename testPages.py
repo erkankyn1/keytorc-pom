35e196667e6ba090c45ad0f63439284cd6744666
Erkan Yıldırım <41471219+erkankyn1@users.noreply.github.com>
2020-04-22 00:08:50 +0300

    Update testPages.py

--- testPages.py
+++ testPages.py
@@ -14,28 +14,29 @@ class TestPages(unittest.TestCase):
         cls.driver.maximize_window()
 
     def test_01_search_samsung_add_favorite_remove_favorite_with_login_valid_user(self):
-        
+
         driver=self.driver
-        # Homepage Check Page Load! -- MainPage(Change to base)
-        Base=MainPage(driver)
-        self.assertTrue(Base.check_page_loaded())
-        Base.click_sign_in_button()
+
+        # Homepage - Check Page Load!
+        Home=HomePage(driver)
+        self.assertTrue(Home.check_page_loaded())
+        Home.click_sign_in_button()
+        
         
         # Login Page
         Login=LoginPage(driver)
-        Login.login_success("yourvalid_user")
+        Login.login("yourvalid_user")
         self.assertEqual("https://www.n11.com/",driver.current_url)
         
-        # Home Page
-        Home=HomePage(driver)
+        # Search In Home Page!
         Home.click_search_item()
-
+        
         # Search Page
         Search=SearchPage(driver)
         print("Searching Samsung word in the Results...")
         Search.check_product_search(Home.SEARCHED_ITEM)
         Search.go_to_second_page()
-        self.assertTrue(Base.check_page_loaded())
+        self.assertTrue(Home.check_page_loaded())
         # Return 3rd Item's name.
         returned3rd_item_name=Search.add_favorites_third_product()
         # Print 3rd Item's name

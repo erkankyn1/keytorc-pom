d3e630931f5ce836177ea809e79d909d3076f717
Erkan Yıldırım <41471219+erkankyn1@users.noreply.github.com>
2020-04-22 11:44:04 +0300

    Update testPages.py

--- testPages.py
+++ testPages.py
@@ -18,45 +18,42 @@ class TestPages(unittest.TestCase):
 
         driver=self.driver
 
-        # Base Class
-        BaseClass=Base(driver)
-
-        # Homepage - Check Page Load!
-        Home=HomePage(driver)
-        Home.check_kvkkmodel_warning()
-        self.assertTrue(Home.check_page_loaded())
-        Home.click_sign_in_button()
+        # homepage - Check Page Load!
+        home=HomePage(driver)
+        home.check_kvkkmodel_warning()
+        self.assertTrue(home.check_page_loaded())
+        home.click_sign_in_button()
         
         # Login Page
-        Login=LoginPage(driver)
-        Login.login("yourvalid_user")
+        login=LoginPage(driver)
+        login.login("yourvalid_user")
         self.assertEqual("https://www.n11.com/",driver.current_url)
         
-        # Search In Home Page!
-        Home.check_kvkkmodel_warning()
-        Home.click_search_item()
+        # Search In home Page!
+        home.check_kvkkmodel_warning()
+        home.click_search_item()
         
         # Search Page
-        Search=SearchPage(driver)
+        search=SearchPage(driver)
         print("Searching 'samsung' word in the Results...")
-        BaseClass.check_product_search(Home.SEARCHED_ITEM)
-        Search.go_to_second_page()
+        home.check_product_search(home.SEARCHED_ITEM)
+        search.go_to_second_page()
         # Return 3rd Item's name.
-        returned3rd_item_name=Search.add_favorites_third_product()
+        returned3rd_item_name=search.add_favorites_third_product()
         # Confirm Loaded 2nd Page.
         self.assertEqual("https://www.n11.com/arama?q=samsung&pg=2",driver.current_url)
         # Print 3rd Item's name
-        Search.already_favorites(Home.DISPLAY_NONE)
-        Search.slide_menu()
+        search.already_favorites(home.DISPLAY_NONE)
+        search.slide_menu()
         
         # Favorites Page
-        Favorites=FavoritesPage(driver)
-        Favorites.click_favorites_link()
-        returned_3rd_item_location=BaseClass.check_product_search(returned3rd_item_name)
-        Favorites.delete_product(returned_3rd_item_location)
+        favorites=FavoritesPage(driver)
+        favorites.click_favorites_link()
+        returned_3rd_item_location=home.check_product_search(returned3rd_item_name)
+        favorites.delete_product(returned_3rd_item_location)
         # Confirm Fav Page Loaded.
         self.assertEqual("https://www.n11.com/hesabim/favorilerim",driver.current_url)
-        BaseClass.check_product_search(returned3rd_item_name)
+        home.check_product_search(returned3rd_item_name)
 
     def tearDown(cls):
         cls.driver.close()
